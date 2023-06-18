3cead33336f3936c5f024c5220eff4b56dcfbb2a
Paulo Oyama <papaloyama@gmail.com>
2023-06-18 20:18:42 -0300

    Add BD

--- server_admin/server_admin_portal.py
+++ server_admin/server_admin_portal.py
@@ -1,32 +1,92 @@
 from concurrent import futures
+import json
 import logging
 import grpc
 import sys
 from common import error
 from base import base_pb2_grpc,base_pb2
+import random
+import socket as sk
 
 # [clients] take the CID as value and the data as value
 clients = {}
 # [products] take the PID as value and the data as value
 products = {}
 
+
+def selectReplica():
+    
+    socket1 = None 
+    socket2 = None
+    
+    num = random.randint(1,3)
+    
+    try:
+        host = sk.gethostname()
+        socket1 = sk.socket()
+        socket1.settimeout(1)
+        socket2 = sk.socket()
+        socket2.settimeout(1)
+        print(num)
+        if num == 1: 
+            socket1.connect((host,30020))
+            socket2.connect((host,30021))
+            print("Sockets Connected: 30020, 30021")
+        elif num == 2: 
+            socket1.connect((host,30022))
+            socket2.connect((host,30023))
+            print("Sockets Connected: 30022, 30023")
+        elif num == 3: 
+            socket1.connect((host,30024))
+            socket2.connect((host,30025))
+            print("Sockets Connected: 30024, 30025")
+    
+    except:
+        print("Error in Connection")
+        sys.exit()
+        
+    return socket1,socket2
+
+
+global sck1 
+global sck2 
+sck1 , sck2 = selectReplica()
+
 class AdminPortalServicer(base_pb2_grpc.AdminPortal):
 
     ###### --------------------------------------------------------------- ###### 
     ######                      Client Functions                           ######
     ###### --------------------------------------------------------------- ######
+        
+
 
     def CreateClient(self,request,target):
         """
             createClient() takes and CID [request.CID], and the data of the client [request.data],
             and add in the dictionary, if exists return error 2 and description, if not return error 0 
         """
+        
+        socket = None
+        
+        if int(request.CID) %2 == 0 :
+            socket = sck1
+        else: 
+            socket = sck2
 
         if request.CID not in clients:
-
-            # rps = mqtt_client.publish('clients', f'CREATE {request.CID} {request.data}')
+            
+            # SENAO ACHOU NA LISTA DE CLIENTES PROCURA NO BANCOS
+            msg = json.dumps({"function":"insert", "key": 'C-' +request.CID, "value": request.data})
+            
+            socket.send(msg.encode())
+            resp = socket.recv(16480)
+            
+            print(resp.decode())
+            
+            clients[request.CID] = request.data
 
             print('Created Client')
+            print(clients)
             return base_pb2.Reply(description=error.Error.noError, error=0)
         else: 
             print('Already in DataBase')
@@ -39,13 +99,35 @@ class AdminPortalServicer(base_pb2_grpc.AdminPortal):
             retrieveClient() takes and CID [request.ID], and check in the dictionary if the CID exist in the keys,
             if not return CID 0 and empty data (standard error), if it's return client
         """
+        socket = None
+        
+        if int(request.ID) %2 == 0 :
+            socket = sck1
+        else: 
+            socket = sck2
+
 
         if request.ID not in clients:
-            print('Client Doesn\'t Exist - Retrieve')
-            return base_pb2.Client(CID='0', data='{}')
+            
+            msg = json.dumps({'function':'read', 'key': 'C-' + str(request.ID), 'value': None})
+            
+            socket.send(msg.encode())
+            resp = socket.recv(16480)
+            print(resp.decode())
+            aux=json.loads(resp.decode())
+
+            print("INFO: ", aux)   
+               
+            if aux["data"] == None:
+                print('Client Doesn\'t Exist - Retrieve')
+                return base_pb2.Client(CID='0', data='{}')
+            else:
+                print(type(aux['data']))
+                clients[request.ID] = aux["data"]
+                return base_pb2.Client(CID=request.ID,data=json.dumps(aux["data"]))
         else: 
             print('Retrieve Client')
-            return base_pb2.Client(CID=request.ID,data=clients[request.ID] )
+            return base_pb2.Client(CID=request.ID,data=clients[request.ID])
         
 
     def UpdateClient(self,request,target):
@@ -54,19 +136,31 @@ class AdminPortalServicer(base_pb2_grpc.AdminPortal):
             and check in the dictionary if CID exists in it, if it exists, update data and return error 0, 
             if it not return error 2 and description. 
         """
-
-        if request.CID not in clients:
-            print('Client Doens\'t Exist - Update')
-            return base_pb2.Reply(description=error.Error.clientNotExist, error=2)
+        
+        if int(request.CID) %2 == 0 :
+            socket = sck1
         else: 
-            # rps = mqtt_client.publish('clients', f'UPDATE {request.CID} {request.data}')
-
-            # if rps.is_published():
-            #     print('Updated Client')
-            # else:
-            #     print('Error sending message')
-
-            return base_pb2.Reply(description=error.Error.noError, error=0)
+            socket = sck2
+        
+        
+        if request.CID not in clients:
+            msg = json.dumps({'function':'read', 'key': 'C-' +str(request.CID), 'value': None})
+            
+            socket.send(msg.encode())
+            resp = socket.recv(16480)
+            aux=json.loads(resp.decode())
+            print(resp.decode())
+            
+            if aux['data'] == None:
+                print('Client Doens\'t Exist - Update')
+                return base_pb2.Reply(description=error.Error.clientNotExist, error=2)
+        
+        msg = json.dumps({'function':'update', 'key': 'C-' +str(request.CID), 'value': request.data})
+        socket.send(msg.encode())
+        resp = socket.recv(16480)
+        clients[request.CID] = request.data
+        print("Updated CID")        
+        return base_pb2.Reply(description=error.Error.noError, error=0)
         
 
     def DeleteClient(self,request,target):
@@ -75,13 +169,35 @@ class AdminPortalServicer(base_pb2_grpc.AdminPortal):
             if it not, return error 2 and description, if it's return error 0 
         """
 
-        if request.ID not in clients:
-            print('Client Doesn\'t Exist')
-            return base_pb2.Reply(description=error.Error.clientNotExist, error=2)
+        if int(request.ID) %2 == 0 :
+            socket = sck1
         else: 
+            socket = sck2
+        
+        #Not Locao
+        if request.ID not in clients:
 
-            # mqtt_client.publish('clients', f'DELETE {request.ID}')
-            return base_pb2.Reply(description=error.Error.noError, error=0)
+            msg = json.dumps({'function':'read', 'key': 'C-' + str(request.ID), 'value': None})
+            
+            socket.send(msg.encode())
+            resp = socket.recv(16480)
+            aux=json.loads(resp.decode())
+            print(resp.decode())
+            
+            #Not in BD
+            if aux['data'] == None:
+                print('Client Doesn\'t Exist')
+                return base_pb2.Reply(description=error.Error.clientNotExist, error=2)
+            
+        else:
+            del clients[request.ID]
+        
+        print(clients)
+        msg = json.dumps({'function':'delete', 'key': 'C-' + str(request.ID), 'value': None})
+        socket.send(msg.encode())
+        resp = socket.recv(16480)
+        
+        return base_pb2.Reply(description=error.Error.noError, error=0)
 
     ###### --------------------------------------------------------------- ###### 
     ######                      Product Functions                          ######
@@ -92,28 +208,61 @@ class AdminPortalServicer(base_pb2_grpc.AdminPortal):
             createProduct() takes and PID [request.PID], and the data of the product [request.data],
             and add in the dictionary, if exists return error 2 and description, if not return error 0 
         """
+        socket = None
+        
+        if int(request.PID) %2 == 0 :
+            socket = sck1
+        else: 
+            socket = sck2
 
         if request.PID not in products:
+            
+            # SENAO ACHOU NA LISTA DE CLIENTES PROCURA NO BANCOS
+            msg = json.dumps({"function":"insert", "key": 'P-' + request.PID, "value": request.data})
+            
+            socket.send(msg.encode())
+            resp = socket.recv(16480)
+            
+            print(resp.decode())
+            
+            products[request.PID] = request.data
 
-            # mqtt_client.publish('products', f'CREATE {request.PID} {request.data}')
+            print('Created Product')
+            print(products)
             return base_pb2.Reply(description=error.Error.noError, error=0)
-        
         else: 
             print('Already in DataBase')
             return base_pb2.Reply(description=error.Error.orderExist, error=2)
+
 
     def RetrieveProduct(self,request,target):
         """
             retrieveProduct() takes a PID [request.ID], and check in the dictionary if the PID exist in the keys,
             if not return PID 0 and empty data (standard error), if it's return product
         """
-        print(products)
-        if request.ID not in products:
+        socket = None
+        
+        if int(request.ID) %2 == 0 :
+            socket = sck1
+        else: 
+            socket = sck2
+
+        msg = json.dumps({'function':'read', 'key': 'P-' + str(request.ID), 'value': None})
+        
+        socket.send(msg.encode())
+        resp = socket.recv(16480)
+        print(resp.decode())
+        aux=json.loads(resp.decode())
+
+        if aux["data"] == None:
+            
             print('Product Doesn\'t Exist - Retrieve')
             return base_pb2.Product(PID='0', data='{}')
-        else: 
-            print('Retrieve Product')
-            return base_pb2.Product(PID=request.ID,data=products[request.ID])
+        else:
+            print(aux['data'])
+            products[request.ID] = aux["data"]
+            return base_pb2.Product(PID=request.ID,data=json.dumps(aux["data"]))
+
 
     def UpdateProduct(self,request,target):
         """
@@ -121,29 +270,65 @@ class AdminPortalServicer(base_pb2_grpc.AdminPortal):
             and check in the dictionary if PID exists in it, if it exists, update data and return error 0, 
             if it not return error 2 and description. 
         """
-
-        if request.PID not in products:
-
-            print('Product Doens\'t Exist - Update')
-            return base_pb2.Reply(description=error.Error.orderNotExist, error=2)
+        if int(request.PID) %2 == 0 :
+            socket = sck1
         else: 
-
-            # mqtt_client.publish('products', f'UPDATE {request.PID} {request.data}')
-            return base_pb2.Reply(description=error.Error.noError, error=0)
+            socket = sck2
+        
+        
+        if request.PID not in products:
+            msg = json.dumps({'function':'read', 'key': 'P-' +str(request.PID), 'value': None})
+            
+            socket.send(msg.encode())
+            resp = socket.recv(16480)
+            aux=json.loads(resp.decode())
+            print(resp.decode())
+            
+            if aux['data'] == None:
+                print('Product Doens\'t Exist - Update')
+                return base_pb2.Reply(description=error.Error.orderNotExist, error=2)
+        
+        msg = json.dumps({'function':'update', 'key': 'P-' +str(request.PID), 'value': request.data})
+        socket.send(msg.encode())
+        resp = socket.recv(16480)
+        products[request.PID] = request.data
+        print("Updated PID")        
+        return base_pb2.Reply(description=error.Error.noError, error=0)
 
     def DeleteProduct(self,request,target):
         """
             deleteProduct() takes and PID [request.ID], and check in the dictionary if the PID exist in the keys,
             if it not, return error 2 and description, if it's return error 0 
         """
-
-        if request.ID not in products:
-            print('Product Doesn\'t Exist')
-            return base_pb2.Reply(description=error.Error.orderNotExist, error=2)
+        if int(request.ID) %2 == 0 :
+            socket = sck1
         else: 
+            socket = sck2
+        
+        #Not Locao
+        if request.ID not in products:
+
+            msg = json.dumps({'function':'read', 'key': 'P-' + str(request.ID), 'value': None})
             
-            # mqtt_client.publish('products', f'DELETE {request.ID}')
-            return base_pb2.Reply(description=error.Error.noError, error=0)
+            socket.send(msg.encode())
+            resp = socket.recv(16480)
+            aux=json.loads(resp.decode())
+            print(resp.decode())
+            
+            #Not in BD
+            if aux['data'] == None:
+                print('Product Doesn\'t Exist')
+                return base_pb2.Reply(description=error.Error.orderNotExist, error=2)
+            
+        else:
+            del products[request.ID]
+        
+        print(products)
+        msg = json.dumps({'function':'delete', 'key': 'P-' + str(request.ID), 'value': None})
+        socket.send(msg.encode())
+        resp = socket.recv(16480)
+        
+        return base_pb2.Reply(description=error.Error.noError, error=0)
 
 def serve(port = '50055'):
 
